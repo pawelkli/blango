cea95a68417ba3d9ede722bed9a0d9b65bfa2857
Paweł Klimowicz <pawel.klimowicz@orange.com>
2022-10-20 08:23:09 +0000

    Finish html frameworks

--- blango/settings.py
+++ blango/settings.py
@@ -58,12 +58,14 @@ MIDDLEWARE = [
     #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+
 ROOT_URLCONF = 'blango.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
